bb11cd954b7c44ae36c408be17a7dad50314d1f7
Marko <markoelez7@gmail.com>
2019-09-04 20:52:26 -0400

    api keys

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,13 +1,14 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
 #import "GoogleMaps/GoogleMaps.h"
+#import "constants.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
-  [GMSServices provideAPIKey: @"AIzaSyDt0StFXRLxPTU7OiJiEQlYxsKFpmqdapU"];
+  [GMSServices provideAPIKey: GOOGLE_MAPS_API_KEY];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
